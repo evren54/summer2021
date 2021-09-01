63c0e906736064b98c789a897a1ef64026a3438b
FATMA <evrenozdemir1730001@gmail.com>
2021-09-01 08:46:07 +0300

    Initial commit

---
+++ src/HashSet/HashSet03.java
@@ -0,0 +1,34 @@
+package HashSet;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.TreeSet;
+public class HashSet03 {
+    private static Object start1;
+
+    /*
+                Soru: Elemanlari Alfabetik sirada dizen ve tekrarsiz bir collection elde etmek isterseniz ne kullanirsiniz
+                Cevap: TreeSet
+                Soru: Ama TreeSet cok yavastir. Nasil bir cozum bulabilirsin?
+                Cevap: HashSet olustururum, elemanlari HashSet'e eklerim.
+                Sonra HashSet'i TreeSet'e ceviririm boylece tekrarsiz ve alfabetik sirada bir collection'a sahip olmus olurum.
+         */
+    public static void main(String[] args) {
+        HashSet<String> hs =new HashSet<>(Arrays.asList("said", "oguz", "levent", "oner", "sedef"));
+        System.out.println(hs);//[sedef, oner, oguz, said, levent]
+        TreeSet<String > ts =new TreeSet<>(hs);
+        System.out.println(ts);//[levent, oguz, oner, said, sedef]  long start1=System.currentTimeMillis();
+        TreeSet<String > ts1 =new TreeSet<>(Arrays.asList("said", "oguz", "levent", "oner", "sedef"));
+        long stop1=System.currentTimeMillis();
+        long start1 = 0;
+        System.out.println("treeSet hizi :"+(stop1-start1));
+        long start=System.currentTimeMillis();
+        HashSet<String> hs1 =new HashSet<>(Arrays.asList("said", "oguz", "levent", "oner", "sedef"));
+        System.out.println(hs);//[sedef, oner, oguz, said, levent]
+        TreeSet<String > ts2 =new TreeSet<>(hs);
+        System.out.println(ts);//[levent, oguz, oner, said, sedef]
+        long stop=System.currentTimeMillis();
+        System.out.println("hashSet ile hizi: "+(stop-start));
+
+    }
+}
